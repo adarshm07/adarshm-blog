de37fa7867af14096fca9c2c8cc852c098c4ee6f
Adarsh M <adarshm954@gmail.com>
2022-10-10 11:37:13 +0530

    ui

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -7,30 +7,24 @@ export default function Navbar() {
     const user = useSelector((state: any) => state.user);
 
     return (
-        <nav className="navbar navbar-expand-lg">
+        <nav className="navbar navbar-expand-lg" style={{ boxShadow: "3px 3px 0px 0px #f9f9f9" }}>
             <div className="container">
                 <div className="d-flex justify-content-between align-items-center w-100">
-                    <a className="navbar-brand" href="#">adarshm</a>
-                    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-                    <div className="" id="navbarNavAltMarkup">
+                    <Link href="/"><a className="navbar-brand" href="#">adarshm</a></Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="d-none d-lg-block" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-item nav-link" href="#">Work</a>
-                            <a className="nav-item nav-link" href="#">Resume</a>
-                            <a className="nav-item nav-link" href="#">Blog</a>
-                            <a className="nav-item nav-link" href="#">Contact</a>
+                            <Link href="/"><a className="nav-item nav-link">Work</a></Link>
+                            <Link href="/"><a className="nav-item nav-link">Resume</a></Link>
+                            <Link href="/"><a className="nav-item nav-link">Blog</a></Link>
+                            <Link href="/"><a className="nav-item nav-link">Contact</a></Link>
+                            {Object.keys(user?.user).length ? <a className="nav-item nav-link login-cta" onClick={() => dispatch(loggedInUser(""))}>Logout</a> : <Link href="/login"><a className="nav-item nav-link login-cta">Login</a></Link>}
                         </div>
                     </div>
                 </div>
             </div>
         </nav>
-        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        //     <Link className="navbar-brand" href="/">Navbar</Link>
-        //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        //         <span className="navbar-toggler-icon"></span>
-        //     </button>
-        //     {Object.keys(user?.user).length ? <button className="btn btn-link" onClick={() => dispatch(loggedInUser(""))}>Logout</button> : <Link href="/login" className="btn btn-primary">Login</Link>}
-        // </nav>
     )
 }
