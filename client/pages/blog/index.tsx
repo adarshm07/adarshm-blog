d228fc66ac58e0ab332d88777527dcf9e1958e96
Adarsh M <adarshm954@gmail.com>
2022-10-24 01:00:28 +0530

    changes

--- client/pages/blog/index.tsx
+++ client/pages/blog/index.tsx
@@ -67,7 +67,7 @@ export default function Blog() {
                                 <div className="card-body">
                                     <h5 className="card-title">{item.title}</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    <a href="#" className="btn btn-link">Read More</a>
                                 </div>
                             </div>
                         </div>
