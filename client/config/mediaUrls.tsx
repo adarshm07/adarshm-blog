9ea6acca657b9ea7d28150d6032934fa22586c86
Adarsh M <adarsh.m@resmed.com.au>
2022-12-21 11:29:21 +0530

    apiurl

--- client/config/mediaUrls.tsx
+++ client/config/mediaUrls.tsx
@@ -1,4 +1,4 @@
 export const ResumeLink = "https://s3.ap-south-1.amazonaws.com/adarshm.com/assets/pdf/ADARSH+M.pdf";
 // export const apiDomain = "http://localhost:4000"
 // export const apiDomain = "https://adarshm-blog-backend.vercel.app"
-export const apiDomain = "https://api.adarshm.com/"
+export const apiDomain = "https://api.adarshm.com"
