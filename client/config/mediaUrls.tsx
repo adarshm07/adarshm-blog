70da0ae5770fc35da41f72dec0cdf3a1d82a366b
Adarsh M <adarsh.m@resmed.com.au>
2022-12-21 09:50:27 +0530

    cors

--- client/config/mediaUrls.tsx
+++ client/config/mediaUrls.tsx
@@ -1,3 +1,3 @@
 export const ResumeLink = "https://s3.ap-south-1.amazonaws.com/adarshm.com/assets/pdf/ADARSH+M.pdf";
 // export const apiDomain = "http://localhost:4000"
-export const apiDomain = "https://adarshm-blog-backend.vercel.app/"
+export const apiDomain = "https://adarshm-blog-backend.vercel.app"
