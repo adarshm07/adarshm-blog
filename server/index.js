af8c2e4f2de48f33e0b19668eff8f59c58ee2e65
Adarsh M <adarsh.m@resmed.com.au>
2022-12-21 10:27:31 +0530

    apiurl

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ import PostRoute from "./routes/post.routes.js";
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
