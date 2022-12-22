f094b2a37d66594d5b83bcf7ce0f9dd8cb8c7d17
Adarsh M <adarsh.m@resmed.com.au>
2022-12-22 20:22:24 +0530

    fix

--- client/store/index.ts
+++ client/store/index.ts
@@ -1,27 +1,8 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user";
-// import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
-
-// redux-persist issue with create sync storage - fix
-import createWebStorage from "redux-persist/lib/storage/createWebStorage";
-
-const createNoopStorage = () => {
-    return {
-        getItem(_key: any) {
-            return Promise.resolve(null);
-        },
-        setItem(_key: any, value: any) {
-            return Promise.resolve(value);
-        },
-        removeItem(_key: any) {
-            return Promise.resolve();
-        },
-    };
-};
-
-const storage = typeof window === "undefined" ? createNoopStorage() : createWebStorage();
 
 const persistConfig = {
     key: 'root',
