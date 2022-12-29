b003e3ababcd8baac97e617f6fa11cdd243b84ce
Adarsh M <adarsh.m@resmed.com.au>
2022-12-29 22:48:57 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+// import { createClient } from "redis";
 const app = express();
+import rateLimit from "express-rate-limit";
 
 // Routes
 import AuthRoute from "./routes/auth.routes.js";
@@ -35,6 +37,17 @@ app.use(cors());
 
 app.use(cookieParser());
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (request, response, next, options) =>
+    response.status(options.statusCode).send(options.message),
+});
+
+app.use(limiter);
+
 app.get("/", (req, res) => {
   res.json({ data: "This is index.js" });
 });
@@ -42,8 +55,16 @@ app.get("/", (req, res) => {
 app.use("/auth", AuthRoute);
 app.use("/posts", PostRoute);
 
+// env variables
 const DB_CONNECT = process.env.DB_CONNECT;
 const PORT = process.env.PORT || 4000;
+// const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+// redis server
+// const client = createClient(REDIS_PORT);
+// client.on("error", (err) => console.log("Redis Client Error", err.message));
+// await client.connect();
+
 mongoose
   .connect(DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log("Server connected.")))
