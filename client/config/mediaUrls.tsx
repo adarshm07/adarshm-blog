92fa19603b5203bcd087be6aff3f0d13135be91c
Adarsh M <adarsh.m@resmed.com.au>
2023-01-03 15:12:56 +0530

    sitemap

--- client/config/mediaUrls.tsx
+++ client/config/mediaUrls.tsx
@@ -1,6 +1,6 @@
 export const ResumeLink = "https://s3.ap-south-1.amazonaws.com/adarshm.com/assets/pdf/ADARSH+M.pdf";
-export const apiDomain = "http://localhost:4000"
-// export const apiDomain = "https://api.adarshm.com"
+// export const apiDomain = "http://localhost:4000"
+export const apiDomain = "https://api.adarshm.com"
 // export const apiDomain = "https://adarshm-blog-production.up.railway.app"
-export const domain = "http://localhost:3000"
-// export const domain = "https://adarshm.com"
+// export const domain = "http://localhost:3000"
+export const domain = "https://adarshm.com"
