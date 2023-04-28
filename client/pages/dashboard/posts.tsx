03cceb80974967ba79cdff655dee0f79d2c1e3ad
Adarsh M <adarshm954@gmail.com>
2023-04-28 22:49:07 +0530

    init

--- client/pages/dashboard/posts.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from "../../components/Layout/DashboardLayout";
-
-export default function Post() {
-    return (
-        <DashboardLayout>
-            
-        </DashboardLayout>
-    )
-}
