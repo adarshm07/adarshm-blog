80eedffecb342d3644b95f8e09630b01a2c1aaf1
Adarsh M <adarshm954@gmail.com>
2023-12-22 23:13:00 +0530

    auth0 added

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -5,22 +5,6 @@ import { applyMiddleware } from "graphql-middleware"
 import dbConnect from '@/utils/db-connect';
 import resolvers from './resolvers';
 import typeDefs from './schema';
-import express from 'express';
-import { auth } from "express-openid-connect"
-
-const app = express();
-
-const config = {
-    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-    baseURL: process.env.AUTH0_BASE_URL,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    secret: process.env.AUTH0_CLIENT_SECRET,
-    authRequired: false,
-    auth0Logout: true,
-}
-
-app.use(auth(config))
-
 const schema: any = makeExecutableSchema({ typeDefs, resolvers })
 
 const schemaMiddleware: any = applyMiddleware(schema)
@@ -31,7 +15,6 @@ const server = new ApolloServer({ schema: schemaMiddleware });
 
 const handler = startServerAndCreateNextHandler(server, {
     context: async (req: any, res: any) => {
-        console.log(req.oidc.isAuthenticated());
         return req;
     },
 });
