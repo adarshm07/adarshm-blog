a7019a5c6891370437ed7e632c41497d5cdb879d
Adarsh M <adarshm954@gmail.com>
2024-08-06 10:57:12 +0530

    app master

--- src/app/api/graphql/resolvers/index.ts
+++
@@ -1,6 +0,0 @@
-import PostResolvers from "./Post.resolver";
-import UserResolvers from "./User.resolver"
-
-const resolvers = [UserResolvers, PostResolvers];
-
-export default resolvers;
--- src/types/HeaderTypes.ts
+++
@@ -1,9 +0,0 @@
-export type HeaderMenuType = {
-    id?: number;
-    title: string;
-    url: string;
-}
-
-export type HeaderType = {
-    logo?: string;
-}
