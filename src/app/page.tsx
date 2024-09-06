8857ec404ddc89958131b045376b38e259566c92
Adarsh M <adarshm954@gmail.com>
2024-09-06 20:55:43 +0530

    link and css changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import { BlogPosts } from '@/app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+      <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
+        Hi, Welcome to my blog!
       </h1>
-      <p className="mb-4">
-        {`I am Adarsh M. A JavaScript developer. Kerala, India.`}
-      </p>
+      <h2 className="mb-8 text-3xl font-semibold tracking-tighter">
+        I am Adarsh M. &nbsp; A JavaScript developer. Kerala, India.
+      </h2>
       <div className="my-8">
+        <h3 className="mb-8 text-2xl font-semibold tracking-tighter border-b border-green-600 w-fit">Blog</h3>
         <BlogPosts />
       </div>
     </section>
