65c45752aa21341ce958e308da733cd90f18814b
Adarsh M <adarshm954@gmail.com>
2025-05-26 18:05:21 +0000

    nodejs

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,6 @@ app.use(express.json());
 
 // Log incoming requests
 app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-  console.log('Headers:', JSON.stringify(req.headers, null, 2));
   if (req.body && Object.keys(req.body).length > 0) {
     console.log('Body:', JSON.stringify(req.body, null, 2));
   }
